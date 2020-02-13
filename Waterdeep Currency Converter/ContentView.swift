5df3ab229bd30f721d0a246129fc250c716f6a59
Josh Boles <jaboles@gmail.com>
2020-02-12 21:45:03 -0500

    Initial Commit

---
+++ Waterdeep Currency Converter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Waterdeep Currency Converter
+//
+//  Created by Josh Boles on 2/12/20.
+//  Copyright © 2020 Josh Boles. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
