4d862a319e741474b43be58c1904513a3c2e70f2
Josh Boles <jaboles@gmail.com>
2020-04-05 19:19:50 -0400

    Change alignment. Swap input to str for realtime update.

--- Waterdeep Currency Converter/ContentView.swift
+++ Waterdeep Currency Converter/ContentView.swift
@@ -10,11 +10,12 @@ import SwiftUI
 
 
 struct ContentView: View {
-  @State var inputValue: Double = 1.0
+  @State var inputValue: String = "1"
   @State private var inputCurrency = 0
   let currencies = ["Copper Nibs", "Silver Shards", "Gold Dragons", "Brass Taols", "Platinum Suns", "Platinum Harbor Moons"]
   
   private var copperNibs: Double {
+    let inputValue = Double(self.inputValue) ?? 0.0
     switch(inputCurrency) {
     case 0:
       return inputValue * 1.0
@@ -34,16 +35,16 @@ struct ContentView: View {
   }
   
   private var silverShards: Double {
-    return Double(copperNibs / 10.0)
+    return Double(copperNibs / 10)
   }
   private var goldDragons: Double {
-    return Double(copperNibs / 100.0)
+    return Double(copperNibs / 100)
   }
   private var brassTaols: Double {
-    return Double(copperNibs / 200.0)
+    return Double(copperNibs / 200)
   }
   private var platinumSuns: Double {
-    return Double(copperNibs / 1000.0)
+    return Double(copperNibs / 1000)
   }
   private var platinumHarborMoons: Double {
      return Double(copperNibs / 5000)
@@ -53,7 +54,7 @@ struct ContentView: View {
     NavigationView {
       Form {
         Section {
-          TextField("Amount", value: $inputValue, formatter: NumberFormatter())
+          TextField("Amount", text: $inputValue)
             .keyboardType(.decimalPad)
           
           Picker("Select currency", selection: $inputCurrency) {
@@ -63,24 +64,57 @@ struct ContentView: View {
           }
         }
 
-        Section(header: Text("Input equivalents")) {
-          Text("\(self.copperNibs.removeZerosFromEnd()) copper nibs")
-          .padding()
-        
-          Text("\(self.silverShards.removeZerosFromEnd()) silver shards")
-          .padding()
-        
-          Text("\(self.goldDragons.removeZerosFromEnd()) gold dragons")
-          .padding()
-        
-          Text("\(self.brassTaols.removeZerosFromEnd()) brass taols")
-          .padding()
-        
-          Text("\(self.platinumSuns.removeZerosFromEnd()) platinum suns")
-          .padding()
-        
-          Text("\(self.platinumHarborMoons.removeZerosFromEnd()) platinum harbor moons")
-          .padding()
+        Section(header: Text("Conversions:")) {
+          HStack{
+            VStack {
+              VStack {
+                Text("\(self.copperNibs.removeZerosFromEnd())")
+                  .bold()
+                Text("copper nibs")
+              }
+              .padding()
+              
+              VStack{
+                Text("\(self.goldDragons.removeZerosFromEnd())")
+                  .bold()
+                Text("gold dragons\n")
+              }
+              .padding()
+              
+              VStack {
+                Text("\(self.platinumSuns.removeZerosFromEnd())")
+                  .bold()
+                Text("platinum suns")
+              }
+              .padding()
+            }
+            
+            Spacer()
+            
+            VStack {
+              VStack {
+                Text("\(self.silverShards.removeZerosFromEnd())")
+                  .bold()
+                Text("silver shards")
+              }
+              .padding()
+              
+              VStack{
+                Text("\(self.brassTaols.removeZerosFromEnd())")
+                  .bold()
+                Text("brass taols")
+              }
+              .padding()
+              
+              VStack {
+                Text("\(self.platinumHarborMoons.removeZerosFromEnd())")
+                  .bold()
+                Text("platinum \nharbor moons")
+                  .multilineTextAlignment(.center)
+              }
+              .padding()
+            }
+          }
         }
       }
       .navigationBarTitle(Text("Waterdeep Currency"))
